4ab37a616ad3f0d7385563c95726fb76e1f0413c
Ilya Fomchenko <ilya.elfact@gmail.com>
2020-09-26 22:28:38 +0300

    загрузка json, объявление функций

---
+++ json.php
@@ -0,0 +1,16 @@
+<?php
+  $json = file_get_contents("http://sknt.ru/job/frontend/data.json");
+  $tarifsJson = json_decode( $json, true);
+  
+  function getGroupsData() {
+    
+  };
+
+  function getGroupData($groupNumber){
+    
+  };
+
+  function getTarifData($tarif) {
+
+  };
+?>
