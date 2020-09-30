c3280010a8a00972c18bf723567cb568fa3db485
Ilya Fomchenko <ilya.elfact@gmail.com>
2020-09-30 18:26:17 +0300

    второй экран

--- json.php
+++ json.php
@@ -2,8 +2,12 @@
   $json = file_get_contents("data.json");
   $tarifsJson = json_decode( $json, true);
 
-  if ($_GET['view'] == 'groups') {
+  if ($_GET["view"] == "groups") {
     getGroupsData();
+  } elseif ($_GET["view"] == "group" && isset($_GET["group"])) {
+    getGroupData($_GET["group"]);
+  } elseif (isset($_GET["groupNum"]) && isset($_GET["tarifNum"])) {
+    getTarifData($_GET["groupNum"], $_GET["tarifNum"]);
   }
   function getGroupsData() {
     global $tarifsJson;
@@ -22,21 +26,47 @@
       $groupsData[$i]["Group price"] = $minPerMonth.' – '.$maxPerMonth;
       if (isset($tarif["free_options"])) {
         foreach ($tarif["free_options"] as $option) {
-          $groupsData[$i]['Group free'][] = $option;
+          $groupsData[$i]["Group free"][] = $option;
         }
       }
       $groupsData[$i]["Group link"] = $tarif["link"];
       $i++;
     }
-    $data = json_encode($groupsData);
+    $data = json_encode($groupsData, JSON_UNESCAPED_UNICODE);
     print_r($data);
   };
 
   function getGroupData($groupNumber){
-    
+    global $tarifsJson;
+    $groupData["Tarif title"] = $tarifsJson["tarifs"][$groupNumber]["title"];
+    $prices = [];
+    $j = 1;
+    for ($i = 0; $i < count($tarifsJson["tarifs"][$groupNumber]["tarifs"]); $i++) {
+      array_push($prices, $tarifsJson["tarifs"][$groupNumber]["tarifs"][$i]["price"]);
+    }
+    $maxPerMonth = min($prices);
+    foreach($tarifsJson["tarifs"][$groupNumber]["tarifs"] as $tarif) {
+      $pricePerMonth = $tarif["price"] / $tarif["pay_period"];
+      $groupData[$j]["Tarif period"] = ($tarif["title"] == $tarifsJson["tarifs"][$groupNumber]["title"]) ? '1 месяц'
+      : str_replace(['(',')', $tarifsJson["tarifs"][$groupNumber]["title"]],'',$tarif["title"]);
+      $groupData[$j]["Tarif price"] = $pricePerMonth;
+      $groupData[$j]["Full price"] = $tarif["price"];
+      if ($pricePerMonth < $tarif["price"]) {
+        $groupData[$j]["Tarif discount"] = ($maxPerMonth - $pricePerMonth) * $tarif["pay_period"];
+      }
+      $j++;
+    }
+    // $data = json_encode($tarifData);
+    $data = json_encode($groupData, JSON_UNESCAPED_UNICODE);
+    print_r($data);
   };
 
-  function getTarifData($tarif) {
-
+  function getTarifData($groupNumber, $tarifNum) {
+    global $tarifsJson;
+    $tarifData["Period"] = $tarifsJson["tarifs"][$groupNumber]["tarifs"][$tarifNum]["pay_period"];
+    $tarifData["Price"] = $tarifsJson["tarifs"][$groupNumber]["tarifs"][$tarifNum]["price"];
+    $tarifData["Price per month"] = $tarifData["Price"] / $tarifData["Period"];
+    $data = json_encode($tarifData, JSON_UNESCAPED_UNICODE);
+    print_r($data);
   };
 ?>
--- script.js
+++ script.js
@@ -2,20 +2,47 @@ let mainPage = new Vue({
   el: '#tarifs',
   data: {
     content: '',
+    show: '',
+    groupNum: '',
+    tarifTitle: '',
   },
   methods: {
     speedCheck(title) {
       if (title.includes('Земля')) return 'tarif__speed_earth';
       if (title.includes('Вода')) return 'tarif__speed_water';
       if (title.includes('Огонь')) return 'tarif__speed_fire';
-    }
-  },
-  mounted() {
-    axios
+    },
+    showGroup(groupNumber) {
+      axios
+        .get('json.php?view=group&group='+groupNumber)
+        .then((response) => {
+          this.content = response.data;
+          this.groupNum = groupNumber;
+          this.tarifTitle = this.content["Tarif title"];
+          delete this.content["Tarif title"];
+          this.show = 'group';
+          console.log(response.data);
+        })
+    },
+    showGroups() {
+      axios
       .get('json.php?view=groups')
       .then((response) => {
         this.content = response.data;
+        this.show = 'groups';
       })
     },
-  }
-)
+    showTarif(groupNum,tarif) {
+      groupNum = mainPage.groupNum;
+      axios
+      .get('json.php?groupNum='+groupNum+'&tarifNum='+tarif)
+      .then((response) => {
+        this.show = 'tarif',
+        this.content = response.data;
+        console.log(response.data);
+      })
+    },
+  },
+  mounted() {
+    this.showGroups();
+  }})
